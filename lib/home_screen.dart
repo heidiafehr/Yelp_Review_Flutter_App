c7e74f06b1ae9d0979d9489ad0192cd9f15fbbe8
Fehr <555043@MGMRESORTS.COM>
2022-06-14 13:06:42 -0700

    Added file home_screen and set up the appbar. Added ellipsis truncation for overflow restaurant names

---
+++ lib/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //examples of text strings to see the ellipsis truncation
+    const String text_example1 = 'This is a reallyyyyy long restaurant nameeee';
+    const String text_example2 = 'Medium Restaurant Name This Is';
+    const String text_example3 = 'Short and Simple';
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Center(
+          child: Text('${text_example3}',
+              style: TextStyle(color: Colors.black),
+              overflow: TextOverflow.ellipsis),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/yelp_review_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class YelpReviewApp extends StatelessWidget {
+  const YelpReviewApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home_screen',
+      routes: {
+        '/home_screen' : (context) => const HomeScreen(),
+      },
+    );
+  }
+}
