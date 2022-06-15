abb0bdb6bed8d20145d0a681d5f44c7a97671800
Fehr <555043@MGMRESORTS.COM>
2022-06-15 09:48:05 -0700

    Fixed the glodens test by adding back the null test in the text in the app bar. Added padding to restaurant title

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -19,24 +19,29 @@ class _HomeScreenState extends State<HomeScreen> {
     //const String textExample2 = 'Medium Restaurant Name This Is';
     //const String textExample3 = 'Short and Simple';
 
-    return Scaffold(
+      return Scaffold(
       appBar: AppBar(
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.white,
-          //sets status bar background to white
-          statusBarIconBrightness: Brightness.dark,
-          //sets text colors to dark color
-          statusBarBrightness: Brightness.light,
-        ),
-        
-        backgroundColor: Colors.white,
-        elevation: 0.0,   //removes drop shadow
-        title: const Center(
-          child: Text('Restaurant name goes here',
-              style: TextStyle(color: Colors.black),
-              overflow: TextOverflow.ellipsis),
-        ),
+      //centered, w/ white background and black text
+      //ellipsis when the text is too long
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        //sets status bar background to white
+        statusBarIconBrightness: Brightness.dark,
+        //sets text colors to dark color
+        statusBarBrightness: Brightness.light,
       ),
+
+      elevation: 0.0,
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      title: Padding(
+        padding: const EdgeInsets.all(44.0),
+        child: Text(widget.appBarTitle ?? 'Restaurant Name Goes Here I',
+            style: const TextStyle(color: Colors.black),
+            overflow: TextOverflow.ellipsis),
+      ),
+      )
     );
   }
 }
+
